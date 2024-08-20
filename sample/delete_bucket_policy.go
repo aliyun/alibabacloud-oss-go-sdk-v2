997f8f1100c0ce444c05f256326ac710242a2135
yangpeng <yp01106192@alibaba-inc.com>
2024-08-20 15:35:24 +0800

    add bucket policy api

---
+++ sample/delete_bucket_policy.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"strings"
+
+	"github.com/aliyun/alibabacloud-oss-go-sdk-v2/oss"
+	"github.com/aliyun/alibabacloud-oss-go-sdk-v2/oss/credentials"
+)
+
+var (
+	region     string
+	bucketName string
+)
+
+func init() {
+	flag.StringVar(&region, "region", "", "The region in which the bucket is located.")
+	flag.StringVar(&bucketName, "bucket", "", "The name of the bucket.")
+}
+
+func main() {
+	flag.Parse()
+	if len(bucketName) == 0 {
+		flag.PrintDefaults()
+		log.Fatalf("invalid parameters, bucket name required")
+	}
+
+	if len(region) == 0 {
+		flag.PrintDefaults()
+		log.Fatalf("invalid parameters, region required")
+	}
+
+	cfg := oss.LoadDefaultConfig().
+		WithCredentialsProvider(credentials.NewEnvironmentVariableCredentialsProvider()).
+		WithRegion(region)
+
+	client := oss.NewClient(cfg)
+
+	request := &oss.DeleteBucketPolicyRequest{
+		Bucket: oss.Ptr(bucketName),
+	}
+	result, err := client.DeleteBucketPolicy(context.TODO(), request)
+	if err != nil {
+		log.Fatalf("failed to delete bucket policy %v", err)
+	}
+
+	log.Printf("delete bucket policy result:%#v\n", result)
+}
