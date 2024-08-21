f484171fd78282e551ff1b69bd8bd2e691195c25
yangpeng <yp01106192@alibaba-inc.com>
2024-08-21 11:02:51 +0800

    add bucket archive direct read api

---
+++ sample/get_bucket_archive_direct_read.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	"github.com/aliyun/alibabacloud-oss-go-sdk-v2/oss"
+	"github.com/aliyun/alibabacloud-oss-go-sdk-v2/oss/credentials"
+)
+
+var (
+	region     string
+	bucketName string
+)
+
+func init() {
+	flag.StringVar(&region, "region", "", "The region in which the bucket is located.")
+	flag.StringVar(&bucketName, "bucket", "", "The name of the bucket.")
+}
+
+func main() {
+	flag.Parse()
+	if len(bucketName) == 0 {
+		flag.PrintDefaults()
+		log.Fatalf("invalid parameters, bucket name required")
+	}
+
+	if len(region) == 0 {
+		flag.PrintDefaults()
+		log.Fatalf("invalid parameters, region required")
+	}
+
+	cfg := oss.LoadDefaultConfig().
+		WithCredentialsProvider(credentials.NewEnvironmentVariableCredentialsProvider()).
+		WithRegion(region)
+
+	client := oss.NewClient(cfg)
+
+	request := &oss.GetBucketArchiveDirectReadRequest{
+		Bucket: oss.Ptr(bucketName),
+	}
+	result, err := client.GetBucketArchiveDirectRead(context.TODO(), request)
+	if err != nil {
+		log.Fatalf("failed to get bucket archive direct read %v", err)
+	}
+
+	log.Printf("get bucket archive direct read result:%#v\n", result)
+}
