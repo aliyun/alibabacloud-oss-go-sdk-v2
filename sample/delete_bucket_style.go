acd01263cde4113cf98eef24a61948bf2c69f52e
yangpeng <yp01106192@alibaba-inc.com>
2024-09-03 14:00:02 +0800

    add bucket style api and modify params for v1 signature

---
+++ sample/delete_bucket_style.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	"github.com/aliyun/alibabacloud-oss-go-sdk-v2/oss"
+	"github.com/aliyun/alibabacloud-oss-go-sdk-v2/oss/credentials"
+)
+
+var (
+	region     string
+	bucketName string
+)
+
+func init() {
+	flag.StringVar(&region, "region", "", "The region in which the bucket is located.")
+	flag.StringVar(&bucketName, "bucket", "", "The name of the bucket.")
+}
+
+func main() {
+	flag.Parse()
+	var styleName = "style name"
+	if len(bucketName) == 0 {
+		flag.PrintDefaults()
+		log.Fatalf("invalid parameters, bucket name required")
+	}
+
+	if len(region) == 0 {
+		flag.PrintDefaults()
+		log.Fatalf("invalid parameters, region required")
+	}
+
+	cfg := oss.LoadDefaultConfig().
+		WithCredentialsProvider(credentials.NewEnvironmentVariableCredentialsProvider()).
+		WithRegion(region)
+
+	client := oss.NewClient(cfg)
+
+	request := &oss.DeleteStyleRequest{
+		Bucket:    oss.Ptr(bucketName),
+		StyleName: oss.Ptr(styleName),
+	}
+	result, err := client.DeleteStyle(context.TODO(), request)
+	if err != nil {
+		log.Fatalf("failed to delete bucket style %v", err)
+	}
+
+	log.Printf("delete bucket style result:%#v\n", result)
+}
---
+++ sample/list_bucket_style.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	"github.com/aliyun/alibabacloud-oss-go-sdk-v2/oss"
+	"github.com/aliyun/alibabacloud-oss-go-sdk-v2/oss/credentials"
+)
+
+var (
+	region     string
+	bucketName string
+)
+
+func init() {
+	flag.StringVar(&region, "region", "", "The region in which the bucket is located.")
+	flag.StringVar(&bucketName, "bucket", "", "The name of the bucket.")
+}
+
+func main() {
+	flag.Parse()
+	if len(region) == 0 {
+		flag.PrintDefaults()
+		log.Fatalf("invalid parameters, region required")
+	}
+
+	cfg := oss.LoadDefaultConfig().
+		WithCredentialsProvider(credentials.NewEnvironmentVariableCredentialsProvider()).
+		WithRegion(region)
+
+	client := oss.NewClient(cfg)
+
+	request := &oss.ListStyleRequest{
+		Bucket: oss.Ptr(bucketName),
+	}
+	result, err := client.ListStyle(context.TODO(), request)
+	if err != nil {
+		log.Fatalf("failed to list bucket style %v", err)
+	}
+
+	log.Printf("list bucket style result:%#v\n", result)
+}
