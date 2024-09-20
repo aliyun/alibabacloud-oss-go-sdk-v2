382c3b3e265be492458ce9c305573a58476adcf8
yangpeng <yp01106192@alibaba-inc.com>
2024-09-20 14:49:34 +0800

    add bucket meta query api

---
+++ sample/open_meta_query.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	"github.com/aliyun/alibabacloud-oss-go-sdk-v2/oss"
+	"github.com/aliyun/alibabacloud-oss-go-sdk-v2/oss/credentials"
+)
+
+var (
+	region     string
+	bucketName string
+)
+
+func init() {
+	flag.StringVar(&region, "region", "", "The region in which the bucket is located.")
+	flag.StringVar(&bucketName, "bucket", "", "The name of the bucket.")
+}
+
+func main() {
+	flag.Parse()
+	if len(bucketName) == 0 {
+		flag.PrintDefaults()
+		log.Fatalf("invalid parameters, bucket name required")
+	}
+
+	if len(region) == 0 {
+		flag.PrintDefaults()
+		log.Fatalf("invalid parameters, region required")
+	}
+
+	if len(objectName) == 0 {
+		flag.PrintDefaults()
+		log.Fatalf("invalid parameters, object name required")
+	}
+
+	cfg := oss.LoadDefaultConfig().
+		WithCredentialsProvider(credentials.NewEnvironmentVariableCredentialsProvider()).
+		WithRegion(region)
+
+	client := oss.NewClient(cfg)
+
+	request := &oss.OpenMetaQueryRequest{
+		Bucket: oss.Ptr(bucketName),
+	}
+	result, err := client.OpenMetaQuery(context.TODO(), request)
+	if err != nil {
+		log.Fatalf("failed to open meta query %v", err)
+	}
+
+	log.Printf("open meta query result:%#v\n", result)
+}
