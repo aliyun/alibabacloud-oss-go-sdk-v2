d24e99c6d7f323b6e9e42554ea76222887cb51c7
walker <yp01106192@alibaba-inc.com>
2024-11-22 14:21:12 +0800

    add access point for object process api

---
+++ sample/list_access_point_for_object_process.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	"github.com/aliyun/alibabacloud-oss-go-sdk-v2/oss"
+	"github.com/aliyun/alibabacloud-oss-go-sdk-v2/oss/credentials"
+)
+
+var (
+	region     string
+	bucketName string
+)
+
+func init() {
+	flag.StringVar(&region, "region", "", "The region in which the bucket is located.")
+	flag.StringVar(&bucketName, "bucket", "", "The name of the bucket.")
+}
+
+func main() {
+	flag.Parse()
+	if len(bucketName) == 0 {
+		flag.PrintDefaults()
+		log.Fatalf("invalid parameters, bucket name required")
+	}
+
+	if len(region) == 0 {
+		flag.PrintDefaults()
+		log.Fatalf("invalid parameters, region required")
+	}
+
+	cfg := oss.LoadDefaultConfig().
+		WithCredentialsProvider(credentials.NewEnvironmentVariableCredentialsProvider()).
+		WithRegion(region)
+
+	client := oss.NewClient(cfg)
+
+	request := &oss.ListAccessPointsForObjectProcessRequest{}
+	result, err := client.ListAccessPointsForObjectProcess(context.TODO(), request)
+	if err != nil {
+		log.Fatalf("failed to list access points for object process %v", err)
+	}
+
+	log.Printf("list access points for object process result:%#v\n", result)
+}
